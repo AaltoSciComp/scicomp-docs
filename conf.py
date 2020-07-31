e1d7ed27cb528476bc8af2ae075eab3ed779dd03
Richard Darst <rkd@zgib.net>
2020-07-31 16:19:40 +0300

    footer: Add explicit "welcome contributions" footer
    - Add a text that says " This site is made for Aalto Scientific
      Computing, but we welcome contributions that make it more useful to
      others, too."  Outside readers shouldn't think this is "us or them",
      while specific to us we can make it useful to others, too.
    - set html_show_sphinx to False to keep the footer from growing too
      long.  Instead, include it in this new paragraph.

--- conf.py
+++ conf.py
@@ -208,6 +208,7 @@ html_static_path = ['_static']
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 #html_show_sphinx = True
+html_show_sphinx = False
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
 #html_show_copyright = True
