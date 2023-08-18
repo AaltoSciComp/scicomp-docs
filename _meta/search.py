3d70d1ff52e5098c18d9f55efe2640f49c198bda
Richard Darst <rkd@zgib.net>
2023-08-18 15:55:59 +0300

    _meta/search: Update searching

--- _meta/search.py
+++ _meta/search.py
@@ -12,21 +12,29 @@ To not re-create the database each time:
     python _meta/search.py serve --db=search.db
 
 Searching: `a+b` means a and b right next to each other.
-
-
-
 """
 
+BASE = 'https://scicomp.aalto.fi/'
+
+import copy
 import json
 import os
 import pathlib
+import re
 import sqlite3
 import sys
+from urllib.parse import unquote
 
 try:
     from bs4 import BeautifulSoup
 except ImportError:
     print("Install pip=beautifulsoup4", file=sys.stderr)
+
+def html2text(s):
+    contents = s.get_text()
+    #contents = ' '.join(contents.split())
+    #contents = re.sub(r'\n+', '\n', contents)
+    return contents
 
 def create(path=":memory:"):
     """Create new database and return the connection object"""
@@ -51,40 +59,65 @@ def create(path=":memory:"):
 
     root = pathlib.Path("_build/dirhtml")
     for file in root.glob('**/*.html'):
-        relpath = file.relative_to(root).parent
+        relpath = str(file.relative_to(root).parent) + '/'
 
         raw = file.open().read()
         contents =  BeautifulSoup(raw, 'html.parser')
         title = contents.title.contents[0]
-        #contents = contents.get_text()
-        contents = contents.find('div', {'class': 'document'}).get_text()
+        body = contents.find('div', {'class': 'document'})
+        body = html2text(body)
+        #conn.execute('INSERT INTO pages VALUES (?, ?, ?)', (relpath, title, body))
+        #if relpath.startswith('triton/tut/array'):
+        #    import pdb; pdb.set_trace()
 
-        contents = ' '.join(contents.split())
+
+        body = contents.findAll('section')
+        print(relpath)
+        for section in body:
+            # skip outer container
+            if 'wy-nav-content-wrap' in section.attrs.get('class', []):
+                continue
+            # Find relative path link
+            relpath2 = relpath
+            id_ = section.attrs.get('id', 'no-section')
+            if id_:
+                relpath2 += '#'+id_
+            # Remove subsections and other unuseful stuff
+            section = copy.copy(section)
+            if x := section.find('footer'):
+                x.clear()
+            if x := section.find('div', {'class': 'rst-footer-buttons'}):
+                x.clear()
+            for subsec in section.findAll('section'):
+                subsec.clear()
+            section_body = html2text(section)
+            #print(f"  {relpath2:50}, {repr(section_body)[:150]}")
+            conn.execute('INSERT INTO pages VALUES (?, ?, ?)', (relpath2, title, section_body))
+
         #print(relpath, repr(contents)[:50])
 
         #f = HTMLFilter()
         #f.feed(raw)
         #print(f.text)
 
-        conn.execute('INSERT INTO pages VALUES (?, ?, ?)', (str(relpath), title, contents))
     conn.commit()
     print("Done: creating database", file=sys.stderr)
     return conn
 
-def search(conn, term, tokens=64, limit=10):
+def search(conn, query, tokens=64, limit=10):
     """Do a single search and return snipets"""
-    #term = f"NEAR({term})"
-    print('Searching:', term)
+    #query = f"NEAR({query})"
+    print('Searching:', query)
     cur = conn.execute(
-        "SELECT path, rank, snippet(pages, 2, '', '', '', ?)"
+        f"SELECT :base||path AS path, rank, snippet(pages, 2, '', '', '', :tokens) AS snipet, body"
         " FROM pages WHERE pages"
-        " MATCH ?"
+        " MATCH :query"
         " ORDER BY rank"
-        " LIMIT ?", (tokens, term, limit))
+        " LIMIT :limit", dict(tokens=tokens, query=query, limit=limit, base=BASE))
     for result in cur.fetchall():
         yield result
 
-def serve(conn):
+def serve(conn, bind=':8000'):
     """Start a HTTP server and return snipets"""
     import http.server
     class Handler(http.server.BaseHTTPRequestHandler):
@@ -92,35 +125,44 @@ def serve(conn):
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
-            terms = self.path.strip('/')
-            terms = ' '.join(terms.split('%20'))
-            #print(terms)
-            data = list(search(conn, terms))
+            if self.path == '/':
+                self.wfile.write(b'{"error": "specify search query as path"}')
+            query = self.path.strip('/')
+            query = unquote(query)
+            #print(query)
+            data = list(search(conn, query))
             self.wfile.write(json.dumps(data).encode())
 
     server_class = http.server.HTTPServer
     handler_class = Handler
-    server_address = ('', 8000)
+    server_address = bind.split(':')[0], int(bind.split(':')[1])
     print(f"Server running on {server_address}", file=sys.stderr)
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
 
 
+def dict_factory(cur, row):
+    """sqlite3 helper to return dicts of each row"""
+    fields = [col[0] for col in cur.description]
+    return {key: value for key, value in zip(fields, row)}
+
 
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--db', default=':memory:')
+parser.add_argument('--bind', default=':8000')
 parser.add_argument('mode')
-parser.add_argument('searchterms', nargs='*')
+parser.add_argument('query', nargs='*')
 args = parser.parse_args()
 
 if args.mode == 'create' or args.db == ':memory:':
     conn = create(args.db)
 else:
     conn = sqlite3.connect(args.db)
+conn.row_factory = dict_factory
 
 if args.mode == 'serve':
-    serve(conn)
+    serve(conn, bind=args.bind)
 if args.mode == 'search':
-    for line in search(conn, " ".join(args.searchterms)):
+    for line in search(conn, " ".join(args.query)):
         print(line)
