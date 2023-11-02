cfaafcc52161ce3bd7f38b5844065daff343af99
Richard Darst <rkd@zgib.net>
2023-11-02 21:05:36 +0200

    _meta/search/: Document things better

--- _meta/search/build.sh
+++ _meta/search/build.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+# This script builds a new docker image.  It's a script
+# because... that's how I first saw it and it saves needing to
+# remember the commands.
+
 set -e
 cd $(dirname $0)
 export DOCKER_BUILDKIT=1  # needed for --mount option
--- _meta/search/redeploy.sh
+++ _meta/search/redeploy.sh
@@ -1,6 +1,18 @@
+#!/bin/bash
+
+# This is our main script that does the entire redeployment.
+
 set -e
 cd $(dirname $0)
+# Build the image using that build script
 sh build.sh
+# Push to harbor.cs.aalto.fi.  You need to have done a `docker login
+# harbor.cs.aalto.fi` first.
 docker push harbor.cs.aalto.fi/aaltorse/scicomp-docs-search:latest
+# Apply the new YAML file.
 kubectl apply -f k8s.yaml
+# Rollout a new version.  if the YAML was changed, this might not be
+# needed, but quite likely the YAML is the same and only the image is
+# updated... so a rollout says to restart everything (and the yaml
+# says "pull on every restart")
 kubectl -n rse rollout restart deployment/scicomp-docs-search
