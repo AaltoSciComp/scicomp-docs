f67b5d5ec81ec76f45f53f99afbc5ae25d284304
Simo Tuomisto <simo.tuomisto@aalto.fi>
2023-11-27 10:42:13 +0200

    triton/apps/orca: Adding a new documentation page for ORCA

---
+++ triton/examples/orca/orca_example.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --time=00:15:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --mem=2G
+#SBATCH --output=orca_example.out
+
+module load orca/4.2.1-openmpi
+
+rm -f water*
+
+cat > water.inp << EOF
+!HF
+!DEF2-SVP
+!PAL4
+* xyz 0 1
+O   0.0000   0.0000   0.0626
+H  -0.7920   0.0000  -0.4973
+H   0.7920   0.0000  -0.4973
+*
+EOF
+
+# Parallel runs need the full path to orca executable
+# Do not use srun as orca will call mpi independently: https://www.orcasoftware.de/tutorials_orca/first_steps/trouble_install.html#using-openmpi
+$(command -v orca) water.inp
